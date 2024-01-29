f6d92a28b60fa054d09e68e9691f9279d7529d93
rafmormor3 <rafamoralesmoreno45@gmail.com>
2024-01-29 09:15:53 +0100

    update

--- src/tienda/addTienda.jsx
+++ src/tienda/addTienda.jsx
@@ -41,7 +41,7 @@ export function AddTienda(){
     function add(){
         e.preventDefault();
         createTienda(tienda).then()
-        //setTienda(t.data)
+        setTienda(t.data)
     }
 
     function edit(e){
